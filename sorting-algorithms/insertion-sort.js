bfd35356aeff651991727131ee01c945b88cf6d4
rowleks <momohrowland@gmail.com>
2024-11-25 09:24:42 +0100

    added insertion sort

---
+++ sorting-algorithms/insertion-sort.js
@@ -0,0 +1,27 @@
+/* 
+Given an array of integers, sort the array 
+
+e.g: 
+    arr = [-6, 20, 8, -2, 4]
+    bubbleSort(arr) = [-6, -2, 4, 8, 20]
+
+*/
+
+
+
+
+function inSort(arr) {
+    for (let i=1; i<arr.length; i++)
+    {
+        let numToInsert = arr[i];
+        let j = i - 1;
+        while (arr[j] > numToInsert && j>=0)
+        {
+            arr[j+1] = arr[j];
+            j--
+        }
+        arr[j+1] = numToInsert;
+    }
+
+    return arr
+}
